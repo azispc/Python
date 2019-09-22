a654ba52d72adf6d0dbed54fc33c5923fde31232
Moh.Azis <aziscs20@gmail.com>
2019-09-22 16:55:44 +0700

    luas lingkaran in function python3

---
+++ luaslingkaran.py
@@ -0,0 +1,12 @@
+import math
+
+phi=math.pi
+
+def luaslingkaran(r):
+ hasil_luas=r*r*phi
+ print ("luas Lingkaran: ""{:.3f}".format(hasil_luas))
+ return
+
+r=int(input("Masukkan Jari-jari Lingkaran: "))
+
+luaslingkaran(r)
